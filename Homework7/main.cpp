2b8d65cfb62fc38ab00c5c47071f80e6bfdb6ad0
SergeyTyurin <bwert@yandex.ru>
2018-03-06 22:16:21 +0300

    добавлен аргумент ком. строки

--- Homework7/main.cpp
+++ Homework7/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include "block.h"
 
-int main() {
-    int n;
-    std::cin>>n;
+int main(int argc, char** argv) {
+    int n = std::atoi(argv[1]);
     Block b(n);
     std::string c;
     while(std::cin>>c)
